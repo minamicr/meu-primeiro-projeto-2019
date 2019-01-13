b3a630d70e294eb5e5388b2876e3cb61536381a6
unknown <minamicr@yahoo.com.br>
2019-01-13 20:59:54 -0200

    teste alteração

--- src/main/java/com/cdminami/api/MeuPrimeiroProjetoApplication.java
+++ src/main/java/com/cdminami/api/MeuPrimeiroProjetoApplication.java
@@ -8,7 +8,7 @@ public class MeuPrimeiroProjetoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
-		System.out.println("executei");
+		System.out.println("executei com git");
 	}
 
 }
