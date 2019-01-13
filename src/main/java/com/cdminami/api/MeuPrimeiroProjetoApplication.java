9ace37fdff7a7b352380c190d0fcd19dc08e465e
unknown <minamicr@yahoo.com.br>
2019-01-13 20:48:21 -0200

    Primeiro projeto spring boot 2019

---
+++ src/main/java/com/cdminami/api/MeuPrimeiroProjetoApplication.java
@@ -0,0 +1,15 @@
+package com.cdminami.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuPrimeiroProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
+		System.out.println("executei");
+	}
+
+}
+
